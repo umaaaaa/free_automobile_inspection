4e866408897cafed8950214c3585cd257d07fe7a
umaaaaa <k-kurakata@hykwlab.org>
2015-10-26 14:26:28 +0900

    fix

---
+++ twkey.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+twitter_key = {
+        "consumer_key"       : '3ijyREamIGKZxM6LQEUodZKp5',
+        "consumer_secret"    : 'K1YT2lSkD38vBDz0of9eIQj1tx7KHOqtnmQFoRGVUqMCAd4NwV',
+        "access_token"       : '2587234754-ASltwQEUTGRMKeKSudNPRpAsT3IHbP1mMSt5I7e',
+        "access_token_secret": 'MiUWHQYsUHTSfbCqb1nMMWlTdcUFRPYho9s3CIAvDraHu'
+        }
