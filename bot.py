51460fefdd7e132f9df50ce5a32d212077cdc085
umaaaaa <k-kurakata@hykwlab.org>
2015-10-26 17:34:11 +0900

    fix

--- bot.py
+++ bot.py
@@ -10,6 +10,5 @@ search_tweets = SearchTweets()
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     search_tweets.fetch_tweet()
-    print 'hello'
 
 sched.start()
--- twkey.py
+++ twkey.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-twitter_key = {
-        "consumer_key"       : '3ijyREamIGKZxM6LQEUodZKp5',
-        "consumer_secret"    : 'K1YT2lSkD38vBDz0of9eIQj1tx7KHOqtnmQFoRGVUqMCAd4NwV',
-        "access_token"       : '2587234754-ASltwQEUTGRMKeKSudNPRpAsT3IHbP1mMSt5I7e',
-        "access_token_secret": 'MiUWHQYsUHTSfbCqb1nMMWlTdcUFRPYho9s3CIAvDraHu'
-        }
+import os
+
+# twitter_key = {
+#         "consumer_key"       : '3ijyREamIGKZxM6LQEUodZKp5',
+#         "consumer_secret"    : 'K1YT2lSkD38vBDz0of9eIQj1tx7KHOqtnmQFoRGVUqMCAd4NwV',
+#         "access_token"       : '2587234754-ASltwQEUTGRMKeKSudNPRpAsT3IHbP1mMSt5I7e',
+#         "access_token_secret": 'MiUWHQYsUHTSfbCqb1nMMWlTdcUFRPYho9s3CIAvDraHu'
+#         }
+
+hoge = os.environ.get('CONSUMER_KEY')
+print hoge
+
+# if __name__ == "__main__":
+#
+#     for env in os.environ:
+#         print env
+#
+#     print "----------------------------------"
+#     print os.environ.get("CONSUMER_KEY")
