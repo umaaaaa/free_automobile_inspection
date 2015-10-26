69af0af904dc6f88079c73f0f958d966cf730c74
umaaaaa <k-kurakata@hykwlab.org>
2015-10-26 13:50:28 +0900

    test用

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@
 # -*- coding:utf-8 -*-
 
 from apscheduler.schedulers.blocking import BlockingScheduler
-from search import SearchTweets
+# from search import SearchTweets
 
 sched = BlockingScheduler()
-search_tweets = SearchTweets()
+# search_tweets = SearchTweets()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    search_tweets.fetch_tweet()
+    # search_tweets.fetch_tweet()
+    print 'hello'
 
 sched.start()
